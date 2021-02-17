f2da700d7572274d7272d6c43b853de8d248bb03
Alesya1710001 <kropka171001@gmail.com>
2021-02-17 22:18:12 +0300

    poster-template added

--- lab_1/movies_poster/poster/urls.py
+++ lab_1/movies_poster/poster/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.movie_list, name='movie_list'),
     path('contact/', views.contact, name='contact'),
     path('about_us/', views.about_us, name='about_us'),
+    path('all_movies/', views.poster, name='all_movies'),
+    path('poster/<int:pk>/', views.movie_detail, name='movie_detail'),
 ]
--- lab_1/movies_poster/poster/views.py
+++ lab_1/movies_poster/poster/views.py
@@ -17,3 +17,16 @@ def about_us(request):
     return render(request, 'poster/about.html')
 
 
+def poster(request):
+    movies = Movie.objects.all()
+    return render(request, 'poster/poster.html', {'movies': movies})
+
+
+def movie_detail(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    genres = Movie.objects.get(pk=pk).genre.all()
+    producers = Movie.objects.get(pk=pk).producer.all()
+    countries = Movie.objects.get(pk=pk).country.all()
+    actors = Movie.objects.get(pk=pk).actor.all()
+    return render(request, 'poster/movie_detail.html', {'movie': movie, 'genres': genres, 'producers': producers,
+                                                        'countries': countries, 'actors': actors})
