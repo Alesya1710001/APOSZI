a32ca32af7f40700e1d71d06a62d206ffbfcaa80
Alesya1710001 <kropka171001@gmail.com>
2021-02-17 20:41:14 +0300

    html-templates added

--- lab_1/movies_poster/poster/urls.py
+++ lab_1/movies_poster/poster/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.movie_list, name='movie_list'),
+    path('contact/', views.contact, name='contact'),
+    path('about_us/', views.about_us, name='about_us'),
 ]
--- lab_1/movies_poster/poster/views.py
+++ lab_1/movies_poster/poster/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from poster.models import Movie
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def movie_list(request):
+    movies = Movie.objects.all()[:3]
+    return render(request, 'poster/movie_list.html', {'movies': movies})
+
+
+def contact(request):
+    return render(request, 'poster/contact.html')
+
+
+def about_us(request):
+    return render(request, 'poster/about.html')
+
+
